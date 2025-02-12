8333931f26bb1cf6aeac459da6f67cfe26088ad7
bosmobosmo <bimo.pangestu09@gmail.com>
2025-02-12 16:21:48 +0700

    Add authentication

--- controllers/__init__.py
+++ controllers/__init__.py
@@ -1,5 +1,21 @@
-from odoo import http
+from odoo import exceptions, http
 
 
 class KedaController(http.Controller):
-    ...
+
+    @http.route(
+        '/auth', auth='none', methods=['POST'], csrf=False, type='json',
+    )
+    def authenticate(self, *args, **post):
+        try:
+            login = post['login']
+            password = post['password']
+            db = post['db']
+        except KeyError:
+            raise exceptions.AccessDenied(
+                "Please provide a login, a password, and a db name"
+            )
+
+        http.request.session.authenticate(db, login, password)
+        res = http.request.env['ir.http'].session_info()
+        return res
